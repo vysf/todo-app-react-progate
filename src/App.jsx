6befc67963a2e2e25523dd3e80cabbe6e11d3104
vysf <yusufumar@student.untan.ac.id>
2023-07-03 10:47:39 +0700

    reisntall vite

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import Todos from './assets/components/Todos'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish Progate React Course',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Have lunch with Guru Domba',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Study React with Ninja Ken',
+      completed: false,
+    },
+  ])
+
+  console.log(todos)
+
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      {/* TodoForm */}
+      {/* 
+        Todos
+          - TodoItem
+      */}
+      <Todos todos={todos} />
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px'
+  },
+  title: {
+    fontSize: '36px'
+  }
+}
+
+export default App
